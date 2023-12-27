74130e1fc0ad4af13cafd9f9ff72aef327d3a93c
Israel.Lohuan <israel.lohuan@equiplex.com.br>
2023-12-27 11:07:19 -0300

    first commit

---
+++ src/main/java/com/estudo/estrutura_de_dados/Estrutura_de_dados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.estudo.estrutura_de_dados;
+
+/**
+ *
+ * @author israel.lohuan
+ */
+public class Estrutura_de_dados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
